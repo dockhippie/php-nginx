4332b4d559c66e17699dd51a0934c7537202edf7
Thomas Boerger <thomas@webhippie.de>
2023-12-11 18:18:30 +0100

    minor: add new minor version 8.3

---
+++ v8.3/overlay/etc/container.d/15-php.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+if [[ "${PHP_SKIP_TEMPLATES}" != "true" ]]; then
+    echo "> writing fpm config"
+    gomplate -V \
+        -o /etc/php83/php-fpm.conf \
+        -f /etc/templates/php-fpm.conf.tmpl
+
+    if [[ $? -ne 0 ]]; then
+        echo "failed to write fpm config"
+        exit 1
+    fi
+
+    echo "> writing php config"
+    gomplate -V \
+        -o /etc/php83/php.ini \
+        -f /etc/templates/php.ini.tmpl
+
+    if [[ $? -ne 0 ]]; then
+        echo "failed to write php config"
+        exit 1
+    fi
+fi
+
+for TEMPLATE in $(find /etc/php83/custom.d -mindepth 1 -type d); do
+    RELATIVENAME=${TEMPLATE//\/etc\/php8\/custom.d\//}
+
+    if [ ! -d "/etc/php83/conf.d/${RELATIVENAME}" ]; then
+        mkdir "/etc/php83/conf.d/${RELATIVENAME}"
+    fi
+done
+
+for TEMAPLTE in $(find /etc/php83/custom.d -mindepth 1 -type f); do
+    RELATIVENAME=${TEMPLATE//\/etc\/php8\/custom.d\//}
+
+    ln -sf \
+        "/etc/php83/conf.d/${RELATIVENAME}" \
+        "/etc/php83/custom.d/${RELATIVENAME}"
+done
+
+true
